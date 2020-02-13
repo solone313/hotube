65c9f9e925033a39ebe495b410d8e73f2759058e
solone313 <solone313@naver.com>
2020-02-13 16:31:23 +0900

    dycrypt password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const someOtherPlaintextPassword = 'not_bacon';
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -27,6 +30,21 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+    //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
     }
 })
 
