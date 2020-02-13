14623589f47c8b725b6e297e89a30909c41781a7
solone313 <solone313@naver.com>
2020-02-13 17:08:27 +0900

    add login process

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express()
 const config = require("./config/key");
 const port = 3000
 const { User } = require("./models/User")
+const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser")
 
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 
@@ -35,3 +36,33 @@ app.post('/register',(req,res) => {
         })
     }
 )
+
+app.post('/login',(req,res) => {
+    //요청된 이메일을 데이터베이스에 있는지 확인한다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        //있다면 비밀번호를 확인.
+        user.comparePassword(req.body.password , (err, isMatch) => {
+            if(!isMatch)
+                return res.json({loginSuccess:false, message: "비밀번호가 틀렸습니다."})
+            //맞다면 토큰생성.
+            user.generateToken((err,user) => {
+                if(err) return res.status(400).send(err);
+
+                // 토큰을 저장한다. 어디에 ? 쿠키
+                    res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id})
+            })
+        })
+    })
+    
+    
+    
+
+})
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const someOtherPlaintextPassword = 'not_bacon';
+const jwt = require('jsonwebtoken');
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -45,8 +45,36 @@ userSchema.pre('save', function(next){
                 next()
             });
         });
+    } else {
+        next()
     }
 })
+
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword == 암호화된 비밀번호
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb) {
+    
+    var user = this;
+    
+    // jwt를 이용해서 token을 생성하기
+    
+    var token = jwt.sign(user._id, 'secretToken')
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null,user)
+    })
+}
+
 
 const User = mongoose.model('User', userSchema)
 
